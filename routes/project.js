18f196b882cb159d1b69e37a3027bae3f7561767
Borguuh <abdrahmanadewale@gmail.com>
2022-04-21 18:06:51 +0100

    handle image upload

--- routes/project.js
+++ routes/project.js
@@ -5,4 +5,10 @@ const router = express.Router();
 // middleware
 import { requireSignin } from "../middlewares";
 
+// controllers
+import { uploadImage } from "../controllers/project";
+
+// image
+router.post("/project/upload-image", uploadImage);
+
 module.exports = router;
